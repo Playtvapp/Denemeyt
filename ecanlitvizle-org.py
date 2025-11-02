9d8f89c513815877d30bffbe68588863da37e9c7
Playtvapp <playtvizleapp@gmail.com>
2025-11-02 13:31:47 +0300

    Update ecanlitvizle-org.py

--- ecanlitvizle-org.py
+++ ecanlitvizle-org.py
@@ -15,7 +15,7 @@ WEB_URL = "https://tv.ecanlitvizle.org/"
 FILE_NAME = "ecanlitvizle-org"
 DOMAIN = "ecanlitvizle.org"
 
-GITHUB_USER = os.getenv("GITHUB_USER", "Playtvapp)
+GITHUB_USER = os.getenv("GITHUB_USER", "Playtvapp")
 GITHUB_REPO = os.getenv("GITHUB_REPO", "Denemeyt")
 GITHUB_BRANCH = os.getenv("GITHUB_BRANCH", "main")
 BASE_URL = f"https://raw.githubusercontent.com/{GITHUB_USER}/{GITHUB_REPO}/refs/heads/{GITHUB_BRANCH}"
